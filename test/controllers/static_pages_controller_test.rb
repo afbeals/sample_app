dc031c0f9bc1cbf19e3ccb94cb319943828885e9
Allan <afbeals@hotmail.com>
2015-04-13 18:52:40 +0000

    finish test for green

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,18 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_response "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_response "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title"
   end
-  
   test "should get about" do
     get :about
     assert_response :success
-    assert_response "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title"
   end
 end
+
